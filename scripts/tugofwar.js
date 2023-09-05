141949740e617246a4c48ecfa8a9b3d42d617275
Fysal Beauferris <fysal.beauferris1@ucalgary.ca>
2023-09-04 23:08:54 -0600

    updated about info section

--- scripts/tugofwar.js
+++ scripts/tugofwar.js
@@ -1,17 +1,16 @@
 //event listener for mobile nav menu
-const menu = document.querySelector('.menu')
-const close = document.querySelector('.close')
-const nav = document.querySelector('.mob-right-nav')
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".mob-right-nav");
 
-menu.addEventListener('click', () => {
-    nav.classList.add('open-nav')
-})
-close.addEventListener('click', () => {
-    nav.classList.remove('open-nav')
-})
-
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
 
 //detect if user is in dark-mode
 const isDarkMode = () => {
-    return window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
+  return window.matchMedia("(prefers-color-scheme: dark)").matches;
+};
